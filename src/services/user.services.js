1d91385d8bd6d069e06bdfe556bdda2898466930
Federico-Taborda <panterx.95@gmail.com>
2025-01-02 22:55:26 -0300

    Feature: User service added

---
+++ src/services/user.services.js
@@ -0,0 +1,34 @@
+import User from '../models/userModel.js';
+
+class UserService {
+    static async createUser(user) {
+        try {
+            const newUser = await User.create(user);
+            return newUser;
+        } catch (error) {
+            if (error.name === 'SequelizeUniqueConstraintError') {
+                console.log('Error: El usuario ya existe.');
+            }
+        }
+    }
+
+    static async getAllUsers() {
+        try {
+            const users = await User.findAll();
+            return users;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async getUserById(id) {
+        try {
+            const user = await User.findByPk(id);
+            return user;
+        }catch (error) {
+            throw error;
+        }
+    }     
+}
+
+export default UserService;
