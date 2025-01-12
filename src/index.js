bf197fbd391c2554004174b8a363d557d21c6ed1
Federico-Taborda <panterx.95@gmail.com>
2025-01-12 16:15:50 -0300

    Chore: move config file to folder

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import app from './app.js';
-import config from './config.js';
+import config from './config/config.js';
 import { sequelize } from './database/connection.js';
 import setupAssociations from './models/associations.js';
 
