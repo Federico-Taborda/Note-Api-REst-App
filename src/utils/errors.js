6d1ff11b775762fa5898ecdc9d9ca488f6d385b1
Federico-Taborda <panterx.95@gmail.com>
2025-01-24 16:52:59 -0300

    Refactor: centralized error

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,91 +1,52 @@
 import httpStatusCode from "./httpStatusCodes.js";
 
-export default class NotFoundError extends Error {
-    constructor(message, detail) {
+class AppError extends Error {
+    constructor(name, message, detail, statusCode) {
         super(message);
+        this.name = name;
         this.detail = detail;
-        this.name = 'NotFoundError'
-        this.statusCode = httpStatusCode.NOT_FOUND;
+        this.statusCode = statusCode;
         this.response = {
-            "success": false,
-            "message": this.message,
-            "detail": this.detail,
-            "status": this.statusCode
-        }
+            success: false,
+            message: this.message,
+            detail: this.detail,
+            status: this.statusCode,
+        };
     }
 }
 
-export class UnauthorizedError extends Error {
+export default class NotFoundError extends AppError {
     constructor(message, detail) {
-        super(message);
-        this.detail = detail;
-        this.name = 'UnauthorizedError'
-        this.statusCode = httpStatusCode.UNAUTHORIZED;
-        this.response = {
-            "success": false,
-            "message": this.message,
-            "detail": this.detail,
-            "status": this.statusCode
-        }
+        super('NotFoundError', message, detail, httpStatusCode.NOT_FOUND);
     }
 }
 
-export class TypeError extends Error {
+export class UnauthorizedError extends AppError {
     constructor(message, detail) {
-        super(message);
-        this.detail = detail;
-        this.name = 'TypeError'
-        this.statusCode = httpStatusCode.BAD_REQUEST;
-        this.response = {
-            "success": false,
-            "message": this.message,
-            "detail": this.detail,
-            "status": this.statusCode
-        }
+        super('UnauthorizedError', message, detail, httpStatusCode.UNAUTHORIZED);
     }
 }
 
-export class ValidationError extends Error {
+export class TypeError extends AppError {
     constructor(message, detail) {
-        super(message);
-        this.detail = detail;
-        this.name = 'ValidationError'
-        this.statusCode = httpStatusCode.BAD_REQUEST;
-        this.response = {
-            "success": false,
-            "message": this.message,
-            "detail": this.detail,
-            "status": this.statusCode
-        }
-    } 
-}
-
-export class InvalidTokenError extends Error {
-    constructor(message, detail) {
-        super(message);
-        this.detail = detail;
-        this.name = 'InvalidTokenError'
-        this.statusCode = httpStatusCode.UNAUTHORIZED;
-        this.response = {
-            "success": false,
-            "message": this.message,
-            "detail": this.detail,
-            "status": this.statusCode
-        }
+        super('TypeError', message, detail, httpStatusCode.BAD_REQUEST);
     }
 }
 
-export class TokenExpiredError extends Error {
+export class ValidationError extends AppError {
     constructor(message, detail) {
-        super(message);
-        this.detail = detail;
-        this.name = 'TokenExpiredError'
-        this.statusCode = httpStatusCode.UNAUTHORIZED;
-        this.response = {
-            "success": false,
-            "message": this.message,
-            "detail": this.detail,
-            "status": this.statusCode
-        }
+        super('ValidationError', message, detail, httpStatusCode.BAD_REQUEST);
+    }
+}
+
+export class InvalidTokenError extends AppError {
+    constructor(message, detail) {
+        super('InvalidTokenError', message, detail, httpStatusCode.UNAUTHORIZED);
+    }
+}
+
+export class TokenExpiredError extends AppError {
+    constructor(message, detail) {
+        super('TokenExpiredError', message, detail, httpStatusCode.UNAUTHORIZED);
     }
 }
